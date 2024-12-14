8a42a15d3c3ab1b2d9e1d9b41d8bf2319451da44
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-12-14 11:42:15 +0100

    feat: add btn link to products-list

--- public/pages/homepage.php
+++ public/pages/homepage.php
@@ -1,6 +1,6 @@
 <h1>Benvenuti</h1>
 <p class="lead">Benvenuti nel sito!</p>
 <p class="lead">Clicca sul bottone per iniziare gli acquisti.</p>
-<a href="#" class="btn btn-primary btn-lg mb-5 mt-3">
+<a href="<?php echo ROOT_URL; ?>shop" class="btn btn-primary btn-lg mb-5 mt-3">
     Vai allo shopping &raquo;
 </a>
