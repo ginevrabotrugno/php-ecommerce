cc6ec8f58f3c15b361071e4ac30ea1237edda371
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-12-16 18:14:13 +0100

    feat: add cart static markup

--- public/template-parts/header.php
+++ public/template-parts/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/style.css">
     <title>PHP Ecommerce</title>
 </head>
@@ -31,6 +32,15 @@
                         </li>
                     </ul>
                     <ul class="navbar-nav ml-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link position-relative mx-3" href="<?php echo ROOT_URL; ?>shop/?page=cart">
+                                <i class="fa-solid fa-cart-shopping fs-4 "></i>
+                                <span class="position-absolute top-5 start-95 translate-middle badge rounded-pill text-bg-success">
+                                    1
+                                    <span class="visually-hidden">unread messages</span>
+                                </span> 
+                            </a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Area Riservata</a>
                             <ul class="dropdown-menu">
--- shop/pages/cart.php
+++ shop/pages/cart.php
@@ -1,2 +1,80 @@
-hello from cart.php
+<div class="order-md-last m-4">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+        <span class="text-primary">Carrello</span>
+        <span class="badge bg-primary rounded-pill">3</span>
+    </h4>
+
+    <ul class="list-group mb-3">
+    <li class="list-group-item d-flex justify-content-between lh-sm p-4">
+            <div class="row w-100 align-items-center justify-content-between">
+                <div class="col-6 col-lg-4">
+                    <h6 class="my-0">Product name</h6>
+                    <small class="text-muted">Brief description</small>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <span class="text-muted">€5</span>
+                </div>
+                <div class="col-6 col-lg-4 text-center">
+                    <div class="cart_btns btn-group" role="group">
+                        <button type="button" class="btn btn-sm btn-primary">-</button>
+                        <span class="text-muted text-center border-top border-bottom my-auto py-1" style="width:50px;">2</span>
+                        <button type="button" class="btn btn-sm btn-primary">+</button>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <strong class="text-primary">€10</strong>
+                </div>
+            </div>
+        </li>
+        <li class="list-group-item d-flex justify-content-between lh-sm p-4">
+            <div class="row w-100 align-items-center justify-content-between">
+                <div class="col-6 col-lg-4">
+                    <h6 class="my-0">Product name</h6>
+                    <small class="text-muted">Brief description</small>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <span class="text-muted">€5</span>
+                </div>
+                <div class="col-6 col-lg-4 text-center">
+                    <div class="cart_btns btn-group" role="group">
+                        <button type="button" class="btn btn-sm btn-primary">-</button>
+                        <span class="text-muted text-center border-top border-bottom my-auto py-1" style="width:50px;">2</span>
+                        <button type="button" class="btn btn-sm btn-primary">+</button>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <strong class="text-primary">€10</strong>
+                </div>
+            </div>
+        </li>
+        <li class="list-group-item d-flex justify-content-between lh-sm p-4">
+            <div class="row w-100 align-items-center justify-content-between">
+                <div class="col-6 col-lg-4">
+                    <h6 class="my-0">Product name</h6>
+                    <small class="text-muted">Brief description</small>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <span class="text-muted">€5</span>
+                </div>
+                <div class="col-6 col-lg-4 text-center">
+                    <div class="cart_btns btn-group" role="group">
+                        <button type="button" class="btn btn-sm btn-primary">-</button>
+                        <span class="text-muted text-center border-top border-bottom my-auto py-1" style="width:50px;">2</span>
+                        <button type="button" class="btn btn-sm btn-primary">+</button>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 text-center">
+                    <strong class="text-primary">€10</strong>
+                </div>
+            </div>
+        </li>
+        <li class="list-group-item d-flex justify-content-between p-4">
+            <span class="fs-4">Totale</span>
+            <strong class="col-6 col-lg-2 text-center text-primary fs-4 mx-3">€30</strong>
+        </li>
+    </ul>
+    
+    <button type="submit" class="btn btn-primary d-block w-100">Checkout</button>
+        
+</div>
 
