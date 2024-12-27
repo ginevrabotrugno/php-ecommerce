cde2faa9a2039f911d5a98671de7535e206d9134
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-12-27 16:22:36 +0100

    feat: query for client in session

--- classes/Cart.php
+++ classes/Cart.php
@@ -2,11 +2,32 @@
 
 class CartManager extends DBManager {
 
+    private $clientId;
+
     public function __construct()
     {
         parent::__construct();
         $this->columns = ['id', 'client'];
         $this->tableName = 'carts';
+
+        $this->_initializeClientIdFromSession();
+    }
+
+    // private methods 
+
+    private function _initializeClientIdFromSession(){
+        if(isset($_SESSION['client'])){
+            $this->clientId = $_SESSION['client'];
+        } else {
+            $str = $this->random_string();
+
+            $_SESSION['client'] = $str;
+            $this->clientId = $str;
+        }
+    }
+
+    private function random_string(){
+        return substr(md5(mt_rand()), 0, 20);
     }
 }
 
